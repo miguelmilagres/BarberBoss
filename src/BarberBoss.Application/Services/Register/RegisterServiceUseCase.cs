8911c867bae5e78af0e30bcb3dc385fdb505db99
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-12 18:06:04 -0300

    feat: adding validation to request save service

--- src/BarberBoss.Application/Services/Register/RegisterServiceUseCase.cs
+++ src/BarberBoss.Application/Services/Register/RegisterServiceUseCase.cs
@@ -1,4 +1,5 @@
-﻿using BarberBoss.Communication.Requests;
+﻿using BarberBoss.Communication.Enums;
+using BarberBoss.Communication.Requests;
 using BarberBoss.Communication.Responses;
 
 namespace BarberBoss.Application.Services.Register
@@ -7,9 +8,35 @@ namespace BarberBoss.Application.Services.Register
     {
         public ResponseRegisteredServiceJson Execute(RequestRegisterServiceJson request)
         {
-            // Validation
+            Validate(request);
 
             return new ResponseRegisteredServiceJson();
+        }
+
+        private void Validate(RequestRegisterServiceJson request)
+        {
+            var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+            if (titleIsEmpty)
+            {
+                throw new ArgumentException("The title is required.");
+            }
+
+            if (request.Price < 0)
+            {
+                throw new ArgumentException("The Price must be greater than zero.");
+            }
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if (result < 0)
+            {
+                throw new ArgumentException("Services cannot be for the future");
+            }
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+            if (paymentTypeIsValid == false)
+            {
+                throw new ArgumentException("Payment Type is not valid.");
+            }
         }
     }
 }
