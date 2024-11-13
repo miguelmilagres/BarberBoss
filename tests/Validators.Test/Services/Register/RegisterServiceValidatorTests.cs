d033e311d0fc2c946847bb1d9ef8200532e0308b
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-13 17:15:53 -0300

    tests: adding our first success test

---
+++ tests/Validators.Test/Services/Register/RegisterServiceValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using BarberBoss.Application.Services.Register;
+using BarberBoss.Communication.Enums;
+using BarberBoss.Communication.Requests;
+
+namespace Validators.Test.Services.Register;
+public class RegisterServiceValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterServiceValidator();
+        var request = new RequestRegisterServiceJson
+        {
+            Title = "Test",
+            Date = DateTime.Now.AddDays(-1),
+            Comment = "Comment",
+            PaymentType = PaymentType.DebitCard,
+            Price = 177
+        };
+
+        var result = validator.Validate(request);
+
+        Assert.True(result.IsValid);
+    }
+}
