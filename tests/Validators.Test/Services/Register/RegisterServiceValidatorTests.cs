3e9d9a0a5fa74dd56727d0135554b6e95cfc541f
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-13 19:23:22 -0300

    tests: all tests for errors and improvements

--- tests/Validators.Test/Services/Register/RegisterServiceValidatorTests.cs
+++ tests/Validators.Test/Services/Register/RegisterServiceValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using BarberBoss.Application.Services.Register;
+using BarberBoss.Communication.Enums;
 using BarberBoss.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
@@ -9,25 +10,77 @@ public class RegisterServiceValidatorTests
     [Fact]
     public void Success()
     {
+        //Arrange
         var validator = new RegisterServiceValidator();
         var request = RequestRegisterServiceJsonBuilder.Build();
 
+        //Act
         var result = validator.Validate(request);
 
+        //Assert
         result.IsValid.Should().BeTrue();
     }
 
-    [Fact]
-    public void Error_Title_Empty()
+    [Theory]
+    [InlineData("")]
+    [InlineData("         ")]
+    [InlineData(null)]
+    public void Error_Title_Empty(string title)
     {
         //Arrange
         var validator = new RegisterServiceValidator();
         var request = RequestRegisterServiceJsonBuilder.Build();
         request.Title = string.Empty;
+        request.Title = title;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+    }
+    [Fact]
+    public void Error_Date_Future()
+    {
+        //Arrange
+        var validator = new RegisterServiceValidator();
+        var request = RequestRegisterServiceJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
         //Act
         var result = validator.Validate(request);
         //Assert
         result.IsValid.Should().BeFalse();
-        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.SERVICES_CANNOT_FOR_THE_FUTURE));
+    }
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        //Arrange
+        var validator = new RegisterServiceValidator();
+        var request = RequestRegisterServiceJsonBuilder.Build();
+        request.PaymentType = (PaymentType)700;
+        //Act
+        var result = validator.Validate(request);
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+    }
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-7)]
+    public void Error_Price_Invalid(decimal price)
+    {
+        //Arrange
+        var validator = new RegisterServiceValidator();
+        var request = RequestRegisterServiceJsonBuilder.Build();
+        request.Price = price;
+        //Act
+        var result = validator.Validate(request);
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PRICE_MUST_BE_GREATER_THAN_ZERO));
     }
 }
