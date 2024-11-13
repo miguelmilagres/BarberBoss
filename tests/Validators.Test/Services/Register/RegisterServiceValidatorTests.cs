dde74401bc7f5644765ebc346377302a1fcc745e
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-13 19:07:40 -0300

    tests: adding error test for service title

--- tests/Validators.Test/Services/Register/RegisterServiceValidatorTests.cs
+++ tests/Validators.Test/Services/Register/RegisterServiceValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using BarberBoss.Application.Services.Register;
+using BarberBoss.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
@@ -14,5 +15,19 @@ public class RegisterServiceValidatorTests
         var result = validator.Validate(request);
 
         result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        //Arrange
+        var validator = new RegisterServiceValidator();
+        var request = RequestRegisterServiceJsonBuilder.Build();
+        request.Title = string.Empty;
+        //Act
+        var result = validator.Validate(request);
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
     }
 }
