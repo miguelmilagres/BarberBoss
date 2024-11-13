ef8cfefec38fda9fe41f0bbf6b241986bb45ae78
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-13 17:44:43 -0300

    tests: adding builder for request register service with bogus

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterServiceJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using BarberBoss.Communication.Enums;
+using BarberBoss.Communication.Requests;
+using Bogus;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterServiceJsonBuilder
+{
+    public static RequestRegisterServiceJson Build()
+    {
+        return new Faker<RequestRegisterServiceJson>()
+            .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(r => r.Comment, faker => faker.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, faker => faker.Date.Past())
+            .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(r => r.Price, faker => faker.Random.Decimal(min: 1, max: 1000));
+    }
+}
