c746e0447cb6d1353c966318b78104486c2952d7
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-13 13:56:42 -0300

    feat: adding and configuring middleware for culture

--- src/BarberBoss.API/Program.cs
+++ src/BarberBoss.API/Program.cs
@@ -1,4 +1,5 @@
 using BarberBoss.API.Filters;
+using BarberBoss.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
