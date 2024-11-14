bf75756c6692e13a47ecdaeeaddd2e8e8c595b29
Miguel Milagres <miguelmdemacedo@gmail.com>
2024-11-14 09:44:14 -0300

    feat: adding service entity in domain

---
+++ src/BarberBoss.Domain/Entities/Service.cs
@@ -0,0 +1,14 @@
+﻿using BarberBoss.Domain.Enums;
+
+namespace BarberBoss.Domain.Entities
+{
+    public class Service
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Comment { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public PaymentType PaymentType { get; set; }
+    }
+}
