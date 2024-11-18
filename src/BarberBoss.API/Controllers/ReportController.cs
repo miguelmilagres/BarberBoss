92a51eb45739366eb4f1b76b626e191d42d31d44
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-18 18:25:04 -0300

    feat: receiving month value

--- src/BarberBoss.API/Controllers/ReportController.cs
+++ src/BarberBoss.API/Controllers/ReportController.cs
@@ -9,7 +9,7 @@ public class ReportController : ControllerBase
     [HttpGet("excel")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> GetExcel()
+    public async Task<IActionResult> GetExcel([FromHeader] DateOnly month)
     {
         byte[] file = new byte[1];
 
