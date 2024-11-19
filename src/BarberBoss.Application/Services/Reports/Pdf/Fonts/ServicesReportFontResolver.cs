f81ddcbb08da1da6730d4fa960232499836580e9
miguelmilagres <miguelmdemacedo@gmail.com>
2024-11-19 08:41:28 -0300

    feat: get bytes for fonts file

--- src/BarberBoss.Application/Services/Reports/Pdf/Fonts/ServicesReportFontResolver.cs
+++ src/BarberBoss.Application/Services/Reports/Pdf/Fonts/ServicesReportFontResolver.cs
@@ -1,15 +1,32 @@
 ﻿using PdfSharp.Fonts;
+using System.Reflection;
 
 namespace BarberBoss.Application.Services.Reports.Pdf.Fonts;
 public class ServicesReportFontResolver : IFontResolver
 {
     public byte[]? GetFont(string faceName)
     {
-        throw new NotImplementedException();
+        var stream = ReadFontFile(faceName);
+
+        if (stream is null)
+            ReadFontFile(FontHelper.DEFAULT_FONT);
+
+        var length = (int)stream!.Length;
+        var data = new byte[length];
+        stream.Read(buffer: data, offset: 0, count: length);
+
+        return data;
     }
 
     public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
     {
         return new FontResolverInfo(familyName);
+    }
+
+    private Stream? ReadFontFile(string faceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return assembly.GetManifestResourceStream($"BarberBoss.Application.Services.Reports.Pdf.Fonts.{faceName}.ttf");
     }
 }
